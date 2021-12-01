9590b85e3e4db2efccadec73df15cf9f0211eea4
nchantre <nelsonfabiancs@hotmail.com>
2021-11-30 21:55:27 -0500

    Configuracion Api
    30/11/2021

---
+++ Hotel/DAL/Models/Hotel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Models
+{
+    public class Hotel
+    {
+        public int IdHotel { get; set; }
+        public string NombreHotel { get; set; }
+        public string PaisHotel { get; set; }
+        public string LatitudHotel { get; set; }
+        public string LongitudHotel { get; set; }
+        public string DescripcionHotel { get; set; }
+        public string ActivoHotel { get; set; }
+        public int NumeroHabitacionesHotel { get; set; }
+
+    }
+}
---
+++ Hotel/DAL/Models/Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Models
+{
+    public class Usuario
+    {
+        public int IdUsuario { get; set; }
+        public string NombreUsuario { get; set; }
+        public string ApellidoUsuario { get; set; }
+        public string EmailUsuario { get; set; }
+        public string DireccionUsuario { get; set; }
+    }
+}
